0f3ff726a13244dbef92972529907dbe8712876c
Edwin Lin <edwinlin@gmail.com>
2018-12-12 19:07:19 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = languages.values
+  new_hash[0].keys.each do |lang| 
+    new_hash[0][lang][:style] = [languages.keys[0]]
+  end
+  new_hash[1].keys.each do |lang| 
+    new_hash[1][lang][:style] = [languages.keys[1]]
+  end
+  
+  style_array = []
+  (new_hash[0].keys & new_hash[1].keys).each do |item|
+    style_array << new_hash[0][item][:style]
+    style_array << new_hash[1][item][:style]
+    new_hash[1][item][:style] = style_array.flatten
+  end
+
+  return new_hash[0].merge(new_hash[1])
+
+
 end
