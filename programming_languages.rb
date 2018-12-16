76b36d798a8fa5e11941c2f816d09d8e6b80c827
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-16 05:14:10 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
   # your code here
+  output = {}
+  
+  languages.each do |key, value|
+    value.each do |k, v|
+      output[k] = {
+        :type => v,
+        :style => [key]
+      }
+  puts output
 end
